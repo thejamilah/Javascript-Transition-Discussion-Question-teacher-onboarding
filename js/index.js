5a3edbd36a88b15799f911ba8963f4b62ebbc4d0
Jamilah McWilliams <jamilah.mcwilliams@flatironschool.com>
2019-03-31 21:17:41 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+//
+// function saveLincoln(interval) {
+//   if(isNaN(interval)) {
+//     interval = 10000
+//   }
+//   $('#foreground').fadeOut(interval);
+// }
 
-function saveLincoln(interval) {
-  if(isNaN(interval)) {
-    interval = 10000
-  }
-  $('#foreground').fadeOut(interval);
-}
+//const is binding of assignment and can't be reassigned
+const saveLincoln = document.getElementById('save_lincoln')
+const timeIinterval = document.getElementById('interval')
+const foregroundFade = document.getElementById('foreground')
+let lincolnMessage = document.getElementById('hidden-message')
